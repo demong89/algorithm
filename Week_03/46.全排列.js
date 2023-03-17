14f4135a5418c07f9959ec21bb8bc05da69574ee
demong <15755881430@163.com>
2023-03-17 13:44:21 +0800

    feat" 46

--- Week_03/46.全排列.js
+++ Week_03/46.全排列.js
@@ -46,10 +46,30 @@ function backtrack(list,temp,nums){
     }
     for (let i = 0; i < nums.length; i++) {
        if (temp.includes(nums[i])) continue;
-       temp.push(nums[i])
+        temp.push(nums[i])
         backtrack(list,temp,nums)
         temp.pop()
     }
 }
 // @lc code=end
+const search = (nodes, path) => {
+    if(path.length === nodes.length){ // 当路径长度等于 nums 数组长度说明这是一个排列
+        result.push([...path]) // 这时候将路径储存
+        return // 推出递归
+    }
+
+    for(let i = 0; i < nodes.length; i++){  // 每次将数组的节点全部遍历一边
+        path.push(nodes[i])   // 这一次遍历需要处理这个节点 首先把值存入路径数组
+        const temp = [...nodes]  
+        temp.splice(i,1) // 这一步和上一步意思是这个节点处理完了 不能用了 把剩下的节点继续递归处理
+        search(temp, path)  // 继续递归
+        path.pop()  // 这个节点递归处理完了之后把它从路径中删除  这里要好好想想
+    }
+}
+const permute = function(nums) {
+    const path = []  // 记录路径
+    const result = []  // 结果
+    search(nums, path) // 开始递归处理
+    return result //返回结果
+};
 
--- test.js
+++ test.js
@@ -1,21 +1,33 @@
 
-function Foo() {
-  this[100] = 'test-100'
-  this[1] = 'test-1'
-  this["B"] = 'bar-B'
-  this[50] = 'test-50'
-  this[9] =  'test-9'
-  this[8] = 'test-8'
-  this[3] = 'test-3'
-  this[5] = 'test-5'
-  this["A"] = 'bar-A'
-  this["C"] = 'bar-C'
-}
-var bar = new Foo()
+// function Foo() {
+//   this[100] = 'test-100'
+//   this[1] = 'test-1'
+//   this["B"] = 'bar-B'
+//   this[50] = 'test-50'
+//   this[9] =  'test-9'
+//   this[8] = 'test-8'
+//   this[3] = 'test-3'
+//   this[5] = 'test-5'
+//   this["A"] = 'bar-A'
+//   this["C"] = 'bar-C'
+// }
+// var bar = new Foo()
 
 
-for(key in bar){
-  console.log(`index:${key}  value:${bar[key]}`)
-}
+// for(key in bar){
+//   console.log(`index:${key}  value:${bar[key]}`)
+// }
 
 // 快慢
+
+
+function rev(num){
+  let newNum = 0
+  while(num){
+    let s = num%10
+    num  =  parseInt(num / 10)
+    newNum = newNum*10 + s
+  } 
+  return newNum
+}
+console.log(rev(12340))
